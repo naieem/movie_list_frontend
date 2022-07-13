3120b40d3f501024a34757651031045c10c23840
Naieem Mahmud Supto <naieemsupto@gmail.com>
2022-07-14 01:31:59 +0700

    routing code added and axios simple config added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginComponent from './components/login.component';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Missing from './components/Missing';
+import RequireAuth from './components/RequireAuth';
+import Unauthorized from './components/Unauthorized';
+
+const ROLES = {
+  'User': 2001,
+  'Editor': 1984,
+  'Admin': 5150
+}
 
 function App() {
   return (
-      <div className="container mx-auto px-4 py-4 justify-center flex h-screen items-center">
-        <LoginComponent></LoginComponent>
-      </div>
+    <div className="container mx-auto px-4 py-4 justify-center flex h-screen items-center">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* public routes */}
+          <Route path="login" element={<LoginComponent />} />
+          <Route path="unauthorized" element={<Unauthorized />} />
+
+          {/* we want to protect these routes */}
+          <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+
+          {/* catch all */}
+          <Route path="*" element={<Missing />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
