4186c2bd2004ebbd22aef98d21c93d56df74b4a0
Naieem Mahmud Supto <naieemsupto@gmail.com>
2022-07-15 03:05:10 +0700

    axios interceptor added for unauthorized access blockage

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,7 +1,19 @@
 import axios from 'axios';
 
-export default axios.create({
+const UserAxios = axios.create({
     baseURL: 'http://localhost:4000',
     withCredentials: true,
     headers: { 'Content-Type': 'application/json' }
 });
+UserAxios.interceptors.response.use(
+    response => {
+        debugger
+        return response.data
+    },
+    error => {
+        debugger
+        if (error.response.data.result.statusCode === 401) {
+            window.location.href = '/login';
+        }
+    });
+export default UserAxios;
