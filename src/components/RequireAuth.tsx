0dc1173dcfc3b722da67f3c12601039a3c58804a
Naieem Mahmud Supto <naieemsupto@gmail.com>
2022-07-17 01:21:14 +0700

    logged in users information saving done from auth component

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,20 +1,23 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import { useEffect, useState } from "react";
 import authService from "../services/auth.service";
+import { useDispatch } from "react-redux";
+import { setLoggedInUserEmail } from "../reducers/authReducer";
 
 const RequireAuth = () => {
     const [userAuth, setuserAuth] = useState({})
     const location = useLocation();
+    const dispatch = useDispatch();
     useEffect(() => {
         (async () => {
             try {
                 const loggedInUser = await authService.getLoggedInUserInfo()
                 setuserAuth(loggedInUser)
-                
+                dispatch(setLoggedInUserEmail(loggedInUser.result.email));
             } catch (error) {
-                
             }
         })()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
