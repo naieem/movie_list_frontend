a3fb3d635bfbab17f5f8dd2aac12d487e27113d2
Naieem Mahmud Supto <naieemsupto@gmail.com>
2022-07-16 18:01:22 +0700

    wrapper added to login component for centrral login form

--- src/components/login.component.tsx
+++ src/components/login.component.tsx
@@ -15,7 +15,7 @@ const LoginComponent = () => {
         try {
             const loginResponse = await authService.login(data);
             console.log(loginResponse);
-            const loggedInUser= await authService.getLoggedInUserInfo();
+            const loggedInUser = await authService.getLoggedInUserInfo();
             navigate("/dashboard", { replace: true });
             debugger
         } catch (error) {
@@ -23,47 +23,50 @@ const LoginComponent = () => {
         }
     }
     return (
-        <div className='w-fill md:w-1/2'>
-            <p className=' text-center font-bold py-5 text-2xl'>Login to your account</p>
-            <Card>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div>
-                        <div className="mb-2 block">
-                            <Label
-                                htmlFor="email1"
-                                value="Your email"
+        <div className="container mx-auto px-4 py-4 justify-center flex h-screen items-center">
+            <div className='w-fill md:w-1/2'>
+                <p className=' text-center font-bold py-5 text-2xl'>Login to your account</p>
+                <Card>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div>
+                            <div className="mb-2 block">
+                                <Label
+                                    htmlFor="email1"
+                                    value="Your email"
+                                />
+                            </div>
+                            <TextInput
+                                id="email1"
+                                type="email"
+                                placeholder="email@email.com"
+                                {...register("email", { required: true })}
                             />
+                            <span className=' text-red-600'>{errors.email && "email is required"}</span>
                         </div>
-                        <TextInput
-                            id="email1"
-                            type="email"
-                            placeholder="email@email.com"
-                            {...register("email", { required: true })}
-                        />
-                        <span className=' text-red-600'>{errors.email && "email is required"}</span>
-                    </div>
-                    <div>
-                        <div className="mb-2 block">
-                            <Label
-                                htmlFor="password1"
-                                value="Your password"
+                        <div>
+                            <div className="mb-2 block">
+                                <Label
+                                    htmlFor="password1"
+                                    value="Your password"
+                                />
+                            </div>
+                            <TextInput
+                                id="password1"
+                                type="password"
+                                {...register("password", { required: true })}
                             />
+                            <span className=' text-red-600'>{errors.password && "Passpord is required"}</span>
                         </div>
-                        <TextInput
-                            id="password1"
-                            type="password"
-                            {...register("password", { required: true })}
-                        />
-                        <span className=' text-red-600'>{errors.password && "Passpord is required"}</span>
-                    </div>
-                    <div className='mt-5 justify-center flex'>
-                        <Button type="submit">
-                            Submit
-                        </Button>
-                    </div>
-                </form>
-            </Card>
+                        <div className='mt-5 justify-center flex'>
+                            <Button type="submit">
+                                Submit
+                            </Button>
+                        </div>
+                    </form>
+                </Card>
+            </div>
         </div>
+
     )
 }
 export default LoginComponent;
